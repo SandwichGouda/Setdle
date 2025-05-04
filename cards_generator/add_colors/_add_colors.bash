532d995393f4bfc350a1063cdb7cefca657b9612
SandwichGouda <william.driot@gmail.com>
2025-05-04 15:48:29 +0200

    Rename files, copy files, prepare duplication script

--- cards_generator/add_colors/_add_colors.bash
+++ cards_generator/add_colors/_add_colors.bash
@@ -1,7 +1,7 @@
-for file in *; do
-    if [[ "$file" == *.svg ]]; then
-        sed -E 's/(fill:)[^;]+;/\1#cb4947;/g' "$file" > "_${file%.svg}r.svg"
-        sed -E 's/(stroke:)[^;]+;/\1#cb4947;/g' "_${file%.svg}r.svg" > "${file%.svg}r.svg"
-        rm "_${file%.svg}r.svg"
-    fi
+for shape in l o z; do
+    for pattern in d e f; do
+        sed 's/000000/cb4947/g' "${shape}${pattern}.svg" > "${shape}${pattern}r.svg"
+        sed 's/000000/469d66/g' "${shape}${pattern}.svg" > "${shape}${pattern}g.svg"
+        sed 's/000000/73397d/g' "${shape}${pattern}.svg" > "${shape}${pattern}p.svg"
+    done
 done
